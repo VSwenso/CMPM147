21fd0d4c6e99fe3d69b4319c05d76f56b897b0a8
VSwenso <vlswenso@ucsc.edu>
2024-04-07 17:59:23 -0700

    glitch content

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Tory Swenson
+// Date: 04/17/24
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
@@ -23,12 +23,56 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    adventurer: ["Charming Stranger", "Aluring Creature", "Enchanting Soul", "Darling", "My Sweet", "Rizzler", "Mommy", "Hot Tamale", "Tempation Incarnate", "Shawty", "Short King", "Pookie Bear", "Cute Sugar Boo", "My Little Sunshine", "Sweetie-Pie"],
+    location: ["Trampoline Park", "Cryptocurrency Forest Meet Up", "LARPing Event", "UFO enthusiast convention", "Bread Aisle", "Taxidermy Shop"],
+    expression: ["poops pants nervously", "bites lip agressively", "nervously approaches with hands clasped together", "sweating profusely", "giggles cutely", "tips fedora", "starts to turn into wolf", "chuggs pepto bismol", "whimpers", "begins to cry", "jaw drops to the floor, eyes pop out of sockets accompanied by trumpets, heart beats out of chest, awooga awooga sound effect, steam blows out of ears", "looks down sheepishly", "wipes comically large bead of sweat from forehead", "clears throat, straightens tie and combs hair"],
+    call: ["bank loan", "beaver", "banana", " Heavily used Rubber Chicken", "light up Kylo Ren funko-pop", "Rabid Wombat", "high-elf with the subclass draconic bloodline", "Minecraft enchanting table", "Cease and Desist", "Crusty white dog that is on the verge of death", "heel of bread loaf that gets uneaten", "peice of candy from the bottom of grandmas purse"],
+    response: ["you've got my interest", "DAMN", "you are aPEELing", "you light up my word", "you are loud and annoying", "I just rolled a D20", "you fire me up", "I am about to go to jail", "you make me think im in heaven"],
+    job: ["Bitcoin", "Subway", "Disney-Pixar", "Only-Fans", "Self-Proclaimed", "Uneducated", "WWE", "dictator", "Dinosaur Impersonator"],
+    title: ["Dark wizard", "sandwich-artist ", "clown", "ultimate mechanic", "interpretive dance intructor", "chief unicorn wrangler","Director of Fun and Frolic", "Grand Poohbah of Affairs", "Time-Lord", "Galatic Ambassador", "Digital Prophet", "Head Schemer"],
+    adjective: ["lovely", "enthralling", "captivating", "radiant", "rizzy", "magnetic", "super-duper", "angelic", "exquisite"],
+    message: ["nanosecond", "moment", "breath", "second", "holler", "twinkling", "bable", "moment of your time", "sec", "..sowwy im shyyy", "...WHY ARE YOU LEAVING", "...wait, no! Come back"],
+    
+  };
+  
+  const template = `Hey there, $adventurer. Mind if I have a quick $message!
+  
+  I couldn't help but notice you from across the $location, Dangggggg *$expression*. Ahem, you look very $adjective. Are you a $call, because $response.
+  
+  I am also a $job, $title. Surely this must tempt one such as yourself!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    box.innerText = story;
+  }
+  
+  /* global clicker */
+  clicker.onclick = generate;
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
