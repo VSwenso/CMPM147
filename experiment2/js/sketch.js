aefa68fc902c35ec071997048f1e88b743915bd0
VSwenso <vlswenso@ucsc.edu>
2024-04-16 19:19:59 -0700

    drawin

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -25,80 +25,6 @@ class MyClass {
         // code to run when method is called
     }
 }
-
-/* exported setup, draw */
-function main() {
-  let seed = 239;
-
-  const grassColor = "#3f539b";
-  //const skyColor = "#3a78c9";
-  const stoneColor = "#08254a";
-  const treeColor = "#046e85";
-
-  function setup() {
-    createCanvas(400, 200);
-    createButton("reimagine").mousePressed(() => seed++);
-  }
-
-  function draw() {
-    randomSeed(seed);
-
-    background(100);
-
-    noStroke();
-
-      let skyTop = color('#08254a');
-      let skyBottom = color('#3a78c9');
-      for (let y = 0; y < height / 2; y++) {
-        let inter = map(y, 0, height / 2, 0, 1);
-        let skyColor = lerpColor(skyTop, skyBottom, inter);
-        stroke(skyColor);
-        line(0, y, width, y);
-      }
-      
-    //fill(skyColor);
-    //rect(0, 0, width, height / 2);
-
-    fill(grassColor);
-    rect(0, height / 2, width, height / 2);
-
-    fill(stoneColor);
-    beginShape();
-    vertex(0, height / 2);
-    const steps = 10;
-    for (let i = 0; i < steps + 1; i++) {
-      let x = (width * i) / steps;
-      let y =
-        height / 2 - (random() * random() * random() * height) / 2;
-      vertex(x, y);
-    }
-    vertex(width, height / 2);
-    endShape(CLOSE);
-  
-    //Draw Bushes
-    const bushes = 50*random() ;
-    const move = mouseX/width
-    fill(treeColor); 
-    for (let i = 0; i < bushes; i++) {
-      let x = width * ((random() + (move/50 + millis() / 500000.0) ) % 1);
-      let y = random(height / 2, height);
-      let size = random(10, 20);
-      ellipse(x, y, size);
-    }
-
-    fill(treeColor);
-    const trees = 20*random();
-    const scrub = mouseX/width;
-    for (let i = 0; i < trees; i++) {
-      let z = random();
-      let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
-      let s = width / 50 / z;
-      let y = height / 2 + height / 20 / z;
-      triangle(x, y - s, x - s / 4, y, x + s / 4, y);
-    }
-  }
-}
-
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
@@ -107,8 +33,19 @@ function resizeScreen() {
   // redrawCanvas(); // Redraw everything based on new size
 }
 
+/* exported setup, draw */
+
+let seed = 239;
+
+const grassColor = "#3f539b";
+//const skyColor = "#3a78c9";
+const stoneColor = "#08254a";
+const treeColor = "#046e85";
+
 // setup() function is called once when the program starts
 function setup() {
+  createCanvas(400, 200);
+  createButton("reimagine").mousePressed(() => seed++);
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
@@ -126,24 +63,61 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  background(100);
+
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  let skyTop = color('#08254a');
+  let skyBottom = color('#3a78c9');
+  for (let y = 0; y < height / 2; y++) {
+    let inter = map(y, 0, height / 2, 0, 1);
+    let skyColor = lerpColor(skyTop, skyBottom, inter);
+    stroke(skyColor);
+    line(0, y, width, y);
+  }
+    
+    //fill(skyColor);
+    //rect(0, 0, width, height / 2);
+
+  fill(grassColor);
+  rect(0, height / 2, width, height / 2);
+
+  fill(stoneColor);
+  beginShape();
+  vertex(0, height / 2);
+  const steps = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    let y =
+      height / 2 - (random() * random() * random() * height) / 2;
+    vertex(x, y);
+  }
+  vertex(width, height / 2);
+  endShape(CLOSE);
+    
+  //Draw Bushes
+  const bushes = 50*random() ;
+  const move = mouseX/width
+  fill(treeColor); 
+  for (let i = 0; i < bushes; i++) {
+    let x = width * ((random() + (move/50 + millis() / 500000.0) ) % 1);
+    let y = random(height / 2, height);
+    let size = random(10, 20);
+    ellipse(x, y, size);
+  }
+
+  fill(treeColor);
+  const trees = 20*random();
+  const scrub = mouseX/width;
+  for (let i = 0; i < trees; i++) {
+    let z = random();
+    let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    let s = width / 50 / z;
+    let y = height / 2 + height / 20 / z;
+    triangle(x, y - s, x - s / 4, y, x + s / 4, y);
+  }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
