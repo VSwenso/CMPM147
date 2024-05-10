326b57a135d2dd88e62724f948fb866a7c0873a7
VSwenso <vlswenso@ucsc.edu>
2024-05-09 22:24:00 -0400

    adjust

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -111,3 +111,77 @@ function draw() {
   
   fpsCounter.innerHTML = Math.round(frameRate());
 }
+
+function p4_inspirations() {
+  return [
+    {
+      name: "rose",
+      assetUrl: "https://cdn.glitch.global/ea12f9d7-b29f-4cea-84f0-e146d84ccc1f/roseimg.webp?v=1715039030735",
+      credit: "https://www.shutterstock.com/image-photo/santa-cruz-beach-boardwalk-wild-california-1334084054"
+    },
+    {
+      name: "google logo",
+      assetUrl: "https://cdn.glitch.global/ea12f9d7-b29f-4cea-84f0-e146d84ccc1f/googimg.png?v=1715039028122",
+      credit: "https://www.ronaldsaunders.com/photo/arch-at-sunset-no-2/"
+    },
+    {
+      name: "apple logo",
+      assetUrl: "https://cdn.glitch.global/ea12f9d7-b29f-4cea-84f0-e146d84ccc1f/appimg.webp?v=1715039033585",
+      credit: "https://www.stevencastrophotography.com/print-shop/walton-lighthouse-santa-cruz-harbor-3546"
+    }
+  ];
+}
+
+function p4_initialize(inspiration) {
+  let scaleFactor = 4;
+  resizeCanvas(inspiration.image.width / scaleFactor, inspiration.image.height / scaleFactor);
+  let design = {
+    bg: 128,
+    shapeType: shapeDropdown.value,
+    shapes: Array(1000).fill().map(() => {
+      let x = random(width);
+      let y = random(height);
+      let col = inspiration.image.get(x * scaleFactor, y * scaleFactor);
+      return {
+        x: x,
+        y: y,
+        w: random(width / 8), // Reduce the maximum width
+        h: random(height / 8), // Reduce the maximum height
+        fill: color(col[0], col[1], col[2])
+      };
+    })
+  };
+  return design;
+}
+
+function p4_render(design, inspiration) {
+  background(design.bg);
+  noStroke();
+  for (let shape of design.shapes) {
+    fill(shape.fill);
+    if (design.shapeType === "rect") {
+      rect(shape.x, shape.y, shape.w, shape.h);
+    } else if (design.shapeType === "ellipse") {
+      ellipse(shape.x, shape.y, shape.w, shape.h);
+    } else if (design.shapeType === "triangle") {
+      triangle(shape.x, shape.y, shape.x + shape.w, shape.y, shape.x, shape.y + shape.h);
+    }
+  }
+}
+
+function mut(num, min, max, rate) {
+  return constrain(randomGaussian(num, (rate * (max - min)) / 20), min, max);
+}
+
+function p4_mutate(design, inspiration, rate) {
+  design.bg = mut(design.bg, 0, 255, rate);
+  let scaleFactor = 4;
+  design.shapes.forEach(shape => {
+    shape.x = mut(shape.x, 0, width, rate);
+    shape.y = mut(shape.y, 0, height, rate);
+    shape.w = mut(shape.w, 0, width / 8, rate); // Reduce the maximum width
+    shape.h = mut(shape.h, 0, height / 8, rate); // Reduce the maximum height
+    let col = inspiration.image.get(shape.x * scaleFactor, shape.y * scaleFactor);
+    shape.fill = color(col[0], col[1], col[2]);
+  });
+}
