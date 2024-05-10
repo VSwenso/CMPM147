b7e777cc03da195bce6f8c2ab5e4584fb3ad8d0b
VSwenso <vlswenso@ucsc.edu>
2024-05-09 23:39:28 -0700

    Exper 6

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -47,6 +47,7 @@ function inspirationChanged(nextInspiration) {
 
 
 function setup() {
+  pixelDensity(1);
   let allInspirations = getInspirations();
   for (let i = 0; i < allInspirations.length; i++) {
     let insp = allInspirations[i];
@@ -61,6 +62,7 @@ function setup() {
   currentInspiration = allInspirations[0];
   restart.onclick = () => inspirationChanged(allInspirations[dropper.value]);
 
+ 
   currentCanvas = createCanvas(width, height);
   currentCanvas.parent(document.getElementById("active"));
   currentScore = Number.NEGATIVE_INFINITY;
@@ -129,4 +131,95 @@ if (nextScore > currentScore) {
     bestScore.innerHTML = currentScore;
 }
 fpsCounter.innerHTML = Math.round(frameRate());
+}
+
+function getInspirations() {
+  return [
+    {
+      name: "Monokuma", 
+      assetUrl: "https://cdn.glitch.global/bc95f201-4fcf-45b7-83d0-664f5d9819d5/thumbnails_raf%2C360x360%2C075%2Ct%2Cfafafa_ca443f4786.jpg?v=1715126603883",
+      credit: "https://images.app.goo.gl/bH72TfuJUx2xChxL7"
+    },
+    {
+      name: "Pringles", 
+      assetUrl: "https://cdn.glitch.global/bc95f201-4fcf-45b7-83d0-664f5d9819d5/images.jpg?v=1715126613749",
+      credit: "https://images.app.goo.gl/v1XjdAyUkCkwaWqR6"
+    },
+    {
+      name: "Minecraft Steve", 
+      assetUrl: "https://cdn.glitch.global/bc95f201-4fcf-45b7-83d0-664f5d9819d5/thumbnails_Steve-Amiibo-6a7f690.jpg?v=1715126609696",
+      credit: "https://images.app.goo.gl/pxoJvViNssD8K2cv7"
+    },
+    {
+      name: "Disaster Girl", 
+      assetUrl: "https://cdn.glitch.global/3abd0223-86fb-43ce-a00a-fde12615bcd5/girl-with-fire.jpg?v=1714778905663",
+      credit: "Four-year-old Zoë Roth, 2005"
+    },
+  ];
+}
+ 
+function initDesign(inspiration) {
+  resizeCanvas(inspiration.image.width / 2, inspiration.image.height / 2);
+  let design = {
+    bg: 128,
+    fg: []
+  };
+  
+  let aspectRatio = inspiration.image.width / inspiration.image.height;
+  let totalRectArea = width * height; // Total area to cover with rectangles
+  let totalInspirationArea = inspiration.image.width * inspiration.image.height;
+  let scaleFactor = 10; // Scale factor to cover canvas
+  
+  let x = 0;
+  let y = 0;
+  
+  while (y < inspiration.image.height) {
+    let fillColor = inspiration.image.get(x, y);
+    design.fg.push({
+      x: x * 2, // Scale coordinates back to original canvas size
+      y: y * 2,
+      w: scaleFactor * aspectRatio,
+      h: scaleFactor,
+      fill: fillColor
+    });
+    
+    x += scaleFactor;
+    if (x >= inspiration.image.width) {
+      x = 0;
+      y += scaleFactor;
+    }
+  }
+  
+  return design;
+}
+function renderDesign(design, inspiration) {
+ // Clear the canvas
+ background(design.bg);
+ noStroke();
+ 
+ // Render foreground objects
+ for(let box of design.fg) {
+     // Get the fill color from the inspiration image
+     let fillColor = inspiration.image.get(box.x / 2, box.y / 2); // Adjusted coordinates
+     fill(fillColor, 128);
+     rect(box.x, box.y, box.w, box.h);
+ }
+}
+
+
+function mutateDesign(design, inspiration, rate) {
+  design.bg = mut(design.bg, 0, 255, rate); // Color mutation for background
+  for(let box of design.fg) {
+      box.x = mut(box.x, 0, width, rate);
+      box.y = mut(box.y, 0, height, rate);
+      box.w = mut(box.w, 0, width/10, rate);
+      box.h = mut(box.h, 0, height/10, rate);
+      // Use inspiration data if needed
+      let fillColor = inspiration.image.get(box.x / 2, box.y / 2); // Adjusted coordinates
+      box.fill = fillColor;
+  }
+}
+
+function mut(num, min, max, rate) {
+  return constrain(randomGaussian(num, (rate * (max - min)) / 20), min, max);
 }
